5e484362b6afb68fe8e051b757ffc9d484f81de2
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-09 21:07:02 +0700

    User Profile Password Change

--- resources/views/frontend/dashboard/user_menu.blade.php
+++ resources/views/frontend/dashboard/user_menu.blade.php
@@ -23,7 +23,7 @@
                     <a href="{{ route('user.profile') }}">User Profile </a>
                 </li>
                 <li>
-                    <a href="#">Change Password</a>
+                    <a href="{{ route('user.change.password') }}">Change Password</a>
                 </li>
                 <li>
                     <a href="#">Booking Details </a>
--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,35 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 
+
 Route::get('/', [UserController::class, 'Index']);
+
 
 Route::get('/dashboard', function () {
     return view('frontend.dashboard.user_dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [UserController::class, 'UserProfile'])->name('user.profile');
     Route::post('/profile/store', [UserController::class, 'UserStore'])->name('profile.store');
     Route::get('/user/logout', [UserController::class, 'UserLogout'])->name('user.logout');
+    Route::get('/user/change/password', [UserController::class, 'UserChangePassword'])->name('user.change.password');
+    Route::post('/password/change/password', [UserController::class, 'ChangePasswordStore'])->name('password.change.store');
+
+
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
 
 // Admin Group Middleware
-Route::middleware(['auth', 'roles:admin'])->group(function () {
+Route::middleware(['auth','roles:admin'])->group(function(){
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+
     Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
     Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
 
@@ -33,3 +41,4 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
 
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
