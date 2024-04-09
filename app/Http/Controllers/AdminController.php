b6e81de5c9601021d1f4ae4330adf19c0a094039
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-09 20:31:12 +0700

    Admin Profile Change Password Part 2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Hash;
 class AdminController extends Controller
 {
     public function AdminDashboard()
@@ -72,6 +72,39 @@ class AdminController extends Controller
         $profileData = User::find($id);
         return view('admin.admin_change_password', compact('profileData'));
     } // End Method
+
+    public function AdminPasswordUpdate(Request $request){
+
+        // Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if(!Hash::check($request->old_password, auth::user()->password)){
+
+            $notification = array(
+                'message' => 'Old Password Does not Match!',
+                'alert-type' => 'error'
+            );
+
+            return back()->with($notification);
+
+        }
+
+        /// Update The New Password
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Change Successfully',
+            'alert-type' => 'success'
+        );
+
+        return back()->with($notification);
+
+    }// End Method
 
 
 
