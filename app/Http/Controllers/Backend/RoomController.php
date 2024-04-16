383d4bffa291c3ab4e5164c268bfefb049152450
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-16 20:13:18 +0700

    Update Rooms Part 3

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -6,22 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Room;
 use App\Models\Facility;
+use App\Models\MultiImage;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
 class RoomController extends Controller
 {
-    public function EditRoom($id)
-    {
+    public function EditRoom($id){
 
-        $basic_facility = Facility::where('rooms_id', $id)->get();
+        $basic_facility = Facility::where('rooms_id',$id)->get();
         $editData = Room::find($id);
-        return view('backend.allroom.rooms.edit_rooms', compact('editData', 'basic_facility'));
+        return view('backend.allroom.rooms.edit_rooms',compact('editData','basic_facility'));
     } //End Method
 
 
-    public function UpdateRoom(Request $request, $id)
-    {
+    public function UpdateRoom(Request $request, $id){
 
         $room  = Room::find($id);
         $room->roomtype_id = $room->roomtype_id;
@@ -38,17 +37,19 @@ class RoomController extends Controller
         $room->description = $request->description;
         /// Update Single Image
 
-        if ($request->file('image')) {
+        if($request->file('image')){
 
-            $image = $request->file('image');
-            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
-            Image::make($image)->resize(550, 850)->save('upload/roomimg/' . $name_gen);
-            $room['image'] = $name_gen;
+        $image = $request->file('image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(550,850)->save('upload/roomimg/'.$name_gen);
+        $room['image'] = $name_gen;
         }
 
         $room->save();
 
-        if ($request->facility_name == NULL) {
+        //// Update for Facility Table
+
+        if($request->facility_name == NULL){
 
             $notification = array(
                 'message' => 'Sorry! Not Any Basic Facility Select',
@@ -56,10 +57,11 @@ class RoomController extends Controller
             );
 
             return redirect()->back()->with($notification);
-        } else {
-            Facility::where('rooms_id', $id)->delete();
+
+        } else{
+            Facility::where('rooms_id',$id)->delete();
             $facilities = Count($request->facility_name);
-            for ($i = 0; $i < $facilities; $i++) {
+            for($i=0; $i < $facilities; $i++ ){
                 $fcount = new Facility();
                 $fcount->rooms_id = $room->id;
                 $fcount->facility_name = $request->facility_name[$i];
@@ -67,8 +69,43 @@ class RoomController extends Controller
             } // end for
         } // end else
 
+        //// Update Multi Image
 
-    } //End Method
+        if($room->save()){
+            $files = $request->multi_img;
+            if(!empty($files)){
+                $subimage = MultiImage::where('rooms_id',$id)->get()->toArray();
+                MultiImage::where('rooms_id',$id)->delete();
+
+            }
+            if(!empty($files)){
+                foreach($files as $file){
+                    $imgName = date('YmdHi').$file->getClientOriginalName();
+                    $file->move('upload/roomimg/multi_img/',$imgName);
+                    $subimage['multi_img'] = $imgName;
+
+                    $subimage = new MultiImage();
+                    $subimage->rooms_id = $room->id;
+                    $subimage->multi_img = $imgName;
+                    $subimage->save();
+                }
+
+            }
+        } // end if
+
+        $notification = array(
+            'message' => 'Room Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    }//End Method
+
+
+
+
 
 
 }
+ 
