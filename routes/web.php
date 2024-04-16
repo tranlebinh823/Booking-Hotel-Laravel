aecba985afad1e3a3fa39d78c6a35e83c00d696d
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-16 20:18:12 +0700

    Show MultiImage In Edit Page Part 2

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,7 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
 
         Route::get('/edit/room/{id}', 'EditRoom')->name('edit.room');
         Route::post('/update/room/{id}', 'UpdateRoom')->name('update.room');
+        Route::get('/multi/image/delete/{id}', 'MultiImageDelete')->name('multi.image.delete');
     }); // End Admin Group Middleware
 
 });
