13f8021a054bf0756be8cd244b1bd41a8ff91cfc
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-17 00:41:52 +0700

    Show Room Details Page in Frontend Part 3

--- app/Http/Controllers/Frontend/FrontendRoomController.php
+++ app/Http/Controllers/Frontend/FrontendRoomController.php
@@ -24,6 +24,7 @@ class FrontendRoomController extends Controller
         return view('frontend.room.room_details', compact('roomdetails'));
         $multiImage = MultiImage::where('rooms_id', $id)->get();
         $facility = Facility::where('rooms_id', $id)->get();
-        return view('frontend.room.room_details', compact('roomdetails', 'multiImage', 'facility'));
+        $otherRooms = Room::where('id','!=', $id)->orderBy('id','DESC')->limit(2)->get();
+        return view('frontend.room.room_details',compact('roomdetails','multiImage','facility','otherRooms'));
     } // End Method
 }
