cc1d661682f1f5a3faad7adf2aef18eb0b15d878
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-17 01:04:57 +0700

    Check Room Details Page Part 6

--- app/Http/Controllers/Frontend/FrontendRoomController.php
+++ app/Http/Controllers/Frontend/FrontendRoomController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\BookArea;
+use App\Models\Booking;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 use App\Models\Room;
@@ -70,5 +71,34 @@ class FrontendRoomController extends Controller
         $otherRooms = Room::where('id', '!=', $id)->orderBy('id', 'DESC')->limit(2)->get();
         $room_id = $id;
         return view('frontend.room.search_room_details', compact('roomdetails', 'multiImage', 'facility', 'otherRooms', 'room_id'));
-    } // End Method 
+    } // End Method
+    public function CheckRoomAvailability(Request $request)
+    {
+
+        $sdate = date('Y-m-d', strtotime($request->check_in));
+        $edate = date('Y-m-d', strtotime($request->check_out));
+        $alldate = Carbon::create($edate)->subDay();
+        $d_period = CarbonPeriod::create($sdate, $alldate);
+        $dt_array = [];
+        foreach ($d_period as $period) {
+            array_push($dt_array, date('Y-m-d', strtotime($period)));
+        }
+
+        $check_date_booking_ids = RoomBookedDate::whereIn('book_date', $dt_array)->distinct()->pluck('booking_id')->toArray();
+
+        $room = Room::withCount('room_numbers')->find($request->room_id);
+
+        $bookings = Booking::withCount('assign_rooms')->whereIn('id', $check_date_booking_ids)->where('rooms_id', $room->id)->get()->toArray();
+
+        $total_book_room = array_sum(array_column($bookings, 'assign_rooms_count'));
+
+        $av_room = @$room->room_numbers_count - $total_book_room;
+
+        $toDate = Carbon::parse($request->check_in);
+        $fromDate = Carbon::parse($request->check_out);
+        $nights = $toDate->diffInDays($fromDate);
+
+        return response()->json(['available_room' => $av_room, 'total_nights' => $nights]);
+    } // End Method
+
 }
