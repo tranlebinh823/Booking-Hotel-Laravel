d029708ce7dbc01d2aea70df487d71f5a73df1df
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-16 20:04:11 +0700

    Edit Room Setup Part 3

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\RoomController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\TeamController;
 use App\Http\Controllers\Backend\RoomTypeController;
@@ -26,14 +27,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/user/logout', [UserController::class, 'UserLogout'])->name('user.logout');
     Route::get('/user/change/password', [UserController::class, 'UserChangePassword'])->name('user.change.password');
     Route::post('/password/change/password', [UserController::class, 'ChangePasswordStore'])->name('password.change.store');
-
-
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 // Admin Group Middleware
-Route::middleware(['auth','roles:admin'])->group(function(){
+Route::middleware(['auth', 'roles:admin'])->group(function () {
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
@@ -42,7 +41,6 @@ Route::middleware(['auth','roles:admin'])->group(function(){
 
     Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
     Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
-
 }); // End Admin Group Middleware
 
 
@@ -50,38 +48,38 @@ Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.
 
 
 // Admin Group Middleware
-Route::middleware(['auth','roles:admin'])->group(function(){
+Route::middleware(['auth', 'roles:admin'])->group(function () {
 
- /// Team All Route
-Route::controller(TeamController::class)->group(function(){
+    /// Team All Route
+    Route::controller(TeamController::class)->group(function () {
 
-    Route::get('/all/team', 'AllTeam')->name('all.team');
-    Route::get('/add/team', 'AddTeam')->name('add.team');
-    Route::post('/team/store', 'StoreTeam')->name('team.store');
-    Route::get('/edit/team/{id}', 'EditTeam')->name('edit.team');
-    Route::post('/team/update', 'UpdateTeam')->name('team.update');
-    Route::get('/delete/team/{id}', 'DeleteTeam')->name('delete.team');
+        Route::get('/all/team', 'AllTeam')->name('all.team');
+        Route::get('/add/team', 'AddTeam')->name('add.team');
+        Route::post('/team/store', 'StoreTeam')->name('team.store');
+        Route::get('/edit/team/{id}', 'EditTeam')->name('edit.team');
+        Route::post('/team/update', 'UpdateTeam')->name('team.update');
+        Route::get('/delete/team/{id}', 'DeleteTeam')->name('delete.team');
+    });
+
+
+    /// Book Area All Route
+    Route::controller(TeamController::class)->group(function () {
+
+        Route::get('/book/area', 'BookArea')->name('book.area');
+        Route::post('/book/area/update', 'BookAreaUpdate')->name('book.area.update');
+    });
+
+    /// RoomType All Route
+    Route::controller(RoomTypeController::class)->group(function () {
+
+        Route::get('/room/type/list', 'RoomTypeList')->name('room.type.list');
+        Route::get('/add/room/type', 'AddRoomType')->name('add.room.type');
+        Route::post('/room/type/store', 'RoomTypeStore')->name('room.type.store');
+    });
+    /// Room All Route
+    Route::controller(RoomController::class)->group(function () {
+
+        Route::get('/edit/room/{id}', 'EditRoom')->name('edit.room');
+    }); // End Admin Group Middleware
 
 });
-
-
- /// Book Area All Route
- Route::controller(TeamController::class)->group(function(){
-
-    Route::get('/book/area', 'BookArea')->name('book.area');
-    Route::post('/book/area/update', 'BookAreaUpdate')->name('book.area.update');
-
-});
-
- /// RoomType All Route
- Route::controller(RoomTypeController::class)->group(function(){
-
-    Route::get('/room/type/list', 'RoomTypeList')->name('room.type.list');
-    Route::get('/add/room/type', 'AddRoomType')->name('add.room.type');
-    Route::post('/room/type/store', 'RoomTypeStore')->name('room.type.store'); 
-});
-
-
-}); // End Admin Group Middleware
-
-
