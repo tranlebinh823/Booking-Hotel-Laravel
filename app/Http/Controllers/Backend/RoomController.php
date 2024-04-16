115cd9e78a301be0cf428c1ffc7b0ce0de4769e9
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-16 20:11:59 +0700

    Update Rooms Part 2

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -3,22 +3,25 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use App\Models\Facility;
 use Illuminate\Http\Request;
 use App\Models\Room;
+use App\Models\Facility;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
 class RoomController extends Controller
 {
-    public function EditRoom($id){
+    public function EditRoom($id)
+    {
 
-        $basic_facility = Facility::where('rooms_id',$id)->get();
+        $basic_facility = Facility::where('rooms_id', $id)->get();
         $editData = Room::find($id);
-        return view('backend.allroom.rooms.edit_rooms',compact('editData','basic_facility'));
+        return view('backend.allroom.rooms.edit_rooms', compact('editData', 'basic_facility'));
     } //End Method
 
-    public function UpdateRoom(Request $request, $id){
+
+    public function UpdateRoom(Request $request, $id)
+    {
 
         $room  = Room::find($id);
         $room->roomtype_id = $room->roomtype_id;
@@ -35,19 +38,37 @@ class RoomController extends Controller
         $room->description = $request->description;
         /// Update Single Image
 
-        if($request->file('image')){
+        if ($request->file('image')) {
 
-        $image = $request->file('image');
-        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-        Image::make($image)->resize(550,850)->save('upload/roomimg/'.$name_gen);
-        $room['image'] = $name_gen;
+            $image = $request->file('image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(550, 850)->save('upload/roomimg/' . $name_gen);
+            $room['image'] = $name_gen;
         }
 
         $room->save();
 
-    }//End Method
+        if ($request->facility_name == NULL) {
+
+            $notification = array(
+                'message' => 'Sorry! Not Any Basic Facility Select',
+                'alert-type' => 'error'
+            );
+
+            return redirect()->back()->with($notification);
+        } else {
+            Facility::where('rooms_id', $id)->delete();
+            $facilities = Count($request->facility_name);
+            for ($i = 0; $i < $facilities; $i++) {
+                $fcount = new Facility();
+                $fcount->rooms_id = $room->id;
+                $fcount->facility_name = $request->facility_name[$i];
+                $fcount->save();
+            } // end for
+        } // end else
 
 
+    } //End Method
 
 
 }
