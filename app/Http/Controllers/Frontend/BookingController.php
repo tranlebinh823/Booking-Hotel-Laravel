7785052d50b04b21490eb6bf1291bd279aff91c3
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-17 01:19:21 +0700

    Cash On Delivery Check Out Option Part 4

--- app/Http/Controllers/Frontend/BookingController.php
+++ app/Http/Controllers/Frontend/BookingController.php
@@ -18,19 +18,18 @@ use Illuminate\Support\Facades\Auth;
 
 class BookingController extends Controller
 {
-    public function Checkout()
-    {
+    public function Checkout(){
 
         if (Session::has('book_date')) {
-            $book_data = Session::get('book_date');
-            $room = Room::find($book_data['room_id']);
+           $book_data = Session::get('book_date');
+           $room = Room::find($book_data['room_id']);
 
-            $toDate = Carbon::parse($book_data['check_in']);
-            $fromDate = Carbon::parse($book_data['check_out']);
-            $nights = $toDate->diffInDays($fromDate);
+           $toDate = Carbon::parse($book_data['check_in']);
+           $fromDate = Carbon::parse($book_data['check_out']);
+           $nights = $toDate->diffInDays($fromDate);
 
-            return view('frontend.checkout.checkout', compact('book_data', 'room', 'nights'));
-        } else {
+           return view('frontend.checkout.checkout',compact('book_data','room','nights'));
+        }else{
 
             $notification = array(
                 'message' => 'Something want to wrong!',
@@ -39,11 +38,10 @@ class BookingController extends Controller
             return redirect('/')->with($notification);
         } // end else
 
-    } // End Method
+    }// End Method
 
 
-    public function BookingStore(Request $request)
-    {
+    public function BookingStore(Request $request){
 
         $validateData = $request->validate([
             'check_in' => 'required',
@@ -67,19 +65,19 @@ class BookingController extends Controller
         $data['number_of_rooms'] = $request->number_of_rooms;
         $data['available_room'] = $request->available_room;
         $data['persion'] = $request->persion;
-        $data['check_in'] = date('Y-m-d', strtotime($request->check_in));
-        $data['check_out'] = date('Y-m-d', strtotime($request->check_out));
+        $data['check_in'] = date('Y-m-d',strtotime($request->check_in));
+        $data['check_out'] = date('Y-m-d',strtotime($request->check_out));
         $data['room_id'] = $request->room_id;
 
-        Session::put('book_date', $data);
+        Session::put('book_date',$data);
 
         return redirect()->route('checkout');
-    } // End Method
 
-    public function CheckoutStore(Request $request)
-    {
+    }// End Method
 
-        $this->validate($request, [
+    public function CheckoutStore(Request $request){
+
+        $this->validate($request,[
             'name' => 'required',
             'email' => 'required',
             'country' => 'required',
@@ -90,50 +88,72 @@ class BookingController extends Controller
             'payment_method' => 'required',
         ]);
 
-        $book_data = Session::get('book_date');
-        $toDate = Carbon::parse($book_data['check_in']);
-        $fromDate = Carbon::parse($book_data['check_out']);
-        $total_nights = $toDate->diffInDays($fromDate);
+           $book_data = Session::get('book_date');
+           $toDate = Carbon::parse($book_data['check_in']);
+           $fromDate = Carbon::parse($book_data['check_out']);
+           $total_nights = $toDate->diffInDays($fromDate);
 
-        $room = Room::find($book_data['room_id']);
-        $subtotal = $room->price * $total_nights * $book_data['number_of_rooms'];
-        $discount = ($room->discount / 100) * $subtotal;
-        $total_price = $subtotal - $discount;
-        $code = rand(000000000, 999999999);
+           $room = Room::find($book_data['room_id']);
+           $subtotal = $room->price * $total_nights * $book_data['number_of_rooms'] ;
+           $discount = ($room->discount/100)*$subtotal;
+           $total_price = $subtotal-$discount;
+           $code = rand(000000000,999999999);
 
-        $data = new Booking();
-        $data->rooms_id = $room->id;
-        $data->user_id = Auth::user()->id;
-        $data->check_in = date('Y-m-d', strtotime($book_data['check_in']));
-        $data->check_out = date('Y-m-d', strtotime($book_data['check_out']));
-        $data->persion = $book_data['persion'];
-        $data->number_of_rooms = $book_data['number_of_rooms'];
-        $data->total_night = $total_nights;
+           $data = new Booking();
+           $data->rooms_id = $room->id;
+           $data->user_id = Auth::user()->id;
+           $data->check_in = date('Y-m-d',strtotime($book_data['check_in']));
+           $data->check_out = date('Y-m-d',strtotime($book_data['check_out']));
+           $data->persion = $book_data['persion'];
+           $data->number_of_rooms = $book_data['number_of_rooms'];
+           $data->total_night = $total_nights;
 
-        $data->actual_price = $room->price;
-        $data->subtotal = $subtotal;
-        $data->discount = $discount;
-        $data->total_price = $total_price;
-        $data->payment_method = $request->payment_method;
-        $data->transation_id = '';
-        $data->payment_status = 0;
+           $data->actual_price = $room->price;
+           $data->subtotal = $subtotal;
+           $data->discount = $discount;
+           $data->total_price = $total_price;
+           $data->payment_method = $request->payment_method;
+           $data->transation_id = '';
+           $data->payment_status = 0;
 
-        $data->name = $request->name;
-        $data->email = $request->email;
-        $data->phone = $request->phone;
-        $data->country = $request->country;
-        $data->state = $request->state;
-        $data->zip_code = $request->zip_code;
-        $data->address = $request->address;
+           $data->name = $request->name;
+           $data->email = $request->email;
+           $data->phone = $request->phone;
+           $data->country = $request->country;
+           $data->state = $request->state;
+           $data->zip_code = $request->zip_code;
+           $data->address = $request->address;
 
-        $data->code = $code;
-        $data->status = 0;
-        $data->created_at = Carbon::now();
-        $data->save();
-    } // End Method
+           $data->code = $code;
+           $data->status = 0;
+           $data->created_at = Carbon::now();
+           $data->save();
+
+        $sdate = date('Y-m-d',strtotime($book_data['check_in']));
+        $edate = date('Y-m-d',strtotime($book_data['check_out']));
+        $eldate = Carbon::create($edate)->subDay();
+        $d_period = CarbonPeriod::create($sdate,$eldate);
+        foreach ($d_period as $period) {
+            $booked_dates = new RoomBookedDate();
+            $booked_dates->booking_id = $data->id;
+            $booked_dates->room_id = $room->id;
+            $booked_dates->book_date = date('Y-m-d', strtotime($period));
+            $booked_dates->save();
+        }
+
+        Session::forget('book_date');
+
+        $notification = array(
+            'message' => 'Booking Added Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect('/')->with($notification);
+
+    }// End Method
 
 
 
 
 
 }
+ 
--- resources/views/frontend/checkout/checkout.blade.php
+++ resources/views/frontend/checkout/checkout.blade.php
@@ -1,212 +1,188 @@
 @extends('frontend.main_master')
 @section('main')
-    <!-- Inner Banner -->
-    <div class="inner-banner inner-bg7">
-        <div class="container">
-            <div class="inner-title">
-                <ul>
-                    <li>
-                        <a href="index.html">Home</a>
-                    </li>
-                    <li><i class='bx bx-chevron-right'></i></li>
-                    <li> Check Out</li>
-                </ul>
-                <h3> Check Out</h3>
-            </div>
+
+ <!-- Inner Banner -->
+ <div class="inner-banner inner-bg7">
+    <div class="container">
+        <div class="inner-title">
+            <ul>
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li><i class='bx bx-chevron-right'></i></li>
+                <li> Check Out</li>
+            </ul>
+            <h3> Check Out</h3>
         </div>
     </div>
-    <!-- Inner Banner End -->
+</div>
+<!-- Inner Banner End -->
 
-    <!-- Checkout Area -->
-    <section class="checkout-area pt-100 pb-70">
-        <div class="container">
+<!-- Checkout Area -->
+<section class="checkout-area pt-100 pb-70">
+    <div class="container">
 
-            <form method="post" role="form" action="{{ route('checkout.store') }}">
-                @csrf
+        <form method="post" role="form" action="{{ route('checkout.store') }}">
+            @csrf
 
-                <div class="row">
-                    <div class="col-lg-8">
-                        <div class="billing-details">
-                            <h3 class="title">Billing Details</h3>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="billing-details">
+                        <h3 class="title">Billing Details</h3>
 
-                            <div class="row">
-                                <div class="col-lg-12 col-md-12">
-                                    <div class="form-group">
-                                        <label>Country <span class="required">*</span></label>
-                                        <div class="select-box">
-                                            <select name="country" class="form-control">
-                                                <option value="Bangladesh">Bangladesh</option>
-                                                <option value="India">India</option>
-                                                <option value="United Arab">United Arab Emirates</option>
-                                                <option value="China">China</option>
-                                                <option value="United Kingdom">United Kingdom</option>
-                                                <option value="Germany">Germany</option>
-                                                <option value="France">France</option>
-                                                <option value="Japan">Japan</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div class="row">
+        <div class="col-lg-12 col-md-12">
+            <div class="form-group">
+                <label>Country <span class="required">*</span></label>
+                <div class="select-box">
+                    <select name="country" class="form-control">
+                        <option value="Bangladesh">Bangladesh</option>
+                        <option value="India">India</option>
+                        <option value="United Arab">United Arab Emirates</option>
+                        <option value="China">China</option>
+                        <option value="United Kingdom">United Kingdom</option>
+                        <option value="Germany">Germany</option>
+                        <option value="France">France</option>
+                        <option value="Japan">Japan</option>
+                    </select>
+                </div>
+            </div>
+        </div>
 
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="form-group">
-                                        <label> Name <span class="required">*</span></label>
-                                        <input type="text" name="name" class="form-control"
-                                            value="{{ \Auth::user()->name }}">
-                                    </div>
-                                </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>  Name <span class="required">*</span></label>
+                <input type="text" name="name" class="form-control" value="{{ \Auth::user()->name }}">
+            </div>
+        </div>
 
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="form-group">
-                                        <label>Email <span class="required">*</span></label>
-                                        <input type="email" name="email" class="form-control"
-                                            value="{{ \Auth::user()->email }}">
-                                    </div>
-                                </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>Email <span class="required">*</span></label>
+                <input type="email" name="email" class="form-control" value="{{ \Auth::user()->email }}">
+            </div>
+        </div>
 
-                                <div class="col-lg-6 col-md-12">
-                                    <div class="form-group">
-                                        <label>Phone</label>
-                                        <input type="text" name="phone" class="form-control"
-                                            value="{{ \Auth::user()->phone }}">
-                                    </div>
-                                </div>
+        <div class="col-lg-6 col-md-12">
+            <div class="form-group">
+                <label>Phone</label>
+                <input type="text" name="phone" class="form-control" value="{{ \Auth::user()->phone }}">
+            </div>
+        </div>
 
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="form-group">
-                                        <label>Address <span class="required">*</span></label>
-                                        <input type="text" name="address" class="form-control"
-                                            value="{{ \Auth::user()->address }}">
-                                    </div>
-                                </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>Address <span class="required">*</span></label>
+                <input type="text" name="address" class="form-control" value="{{ \Auth::user()->address }}">
+            </div>
+        </div>
 
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="form-group">
-                                        <label>State <span class="required">*</span></label>
-                                        <input type="text" name="state" class="form-control">
-                                        @if ($errors->has('state'))
-                                            <div class="text-danger">{{ $errors->first('state') }}</div>
-                                        @endif
-                                    </div>
-                                </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>State <span class="required">*</span></label>
+                <input type="text" name="state" class="form-control">
+                @if ($errors->has('state'))
+                    <div class="text-danger">{{ $errors->first('state') }}</div>
+                @endif
+            </div>
+        </div>
 
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="form-group">
-                                        <label>ZipCode <span class="required">*</span></label>
-                                        <input type="text" name="zip_code" class="form-control">
-                                        @if ($errors->has('zip_code'))
-                                            <div class="text-danger">{{ $errors->first('zip_code') }}</div>
-                                        @endif
-                                    </div>
-                                </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>ZipCode <span class="required">*</span></label>
+                <input type="text" name="zip_code" class="form-control">
+                @if ($errors->has('zip_code'))
+                    <div class="text-danger">{{ $errors->first('zip_code') }}</div>
+                @endif
+            </div>
+        </div>
 
 
-                                <p>Session Value : {{ json_encode(session('book_date')) }}</p>
+    {{-- <p>Session Value : {{ json_encode(session('book_date')) }}</p>   --}}
 
-
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="col-lg-4">
-                        <section class="checkout-area pb-70">
-                            <div class="card-body">
-                                <div class="billing-details">
-                                    <h3 class="title">Booking Summary</h3>
-                                    <hr>
-
-                                    <div style="display: flex">
-                                        <img style="height:100px; width:120px;object-fit: cover"
-                                            src="{{ !empty($room->image) ? url('upload/roomimg/' . $room->image) : url('upload/no_image.jpg') }}"
-                                            alt="Images" alt="Images">
-                                        <div style="padding-left: 10px;">
-                                            <a href=" "
-                                                style="font-size: 20px; color: #595959;font-weight: bold">{{ @$room->type->name }}</a>
-                                            <p><b>{{ $room->price }} / Night</b></p>
-                                        </div>
-
-                                    </div>
-
-                                    <br>
-
-                                    <table class="table" style="width: 100%">
-                                        @php
-                                            $subtotal = $room->price * $nights * $book_data['number_of_rooms'];
-                                            $discount = ($room->discount / 100) * $subtotal;
-                                        @endphp
-
-                                        <tr>
-                                            <td>
-                                                <p>Total Night <br> <b> ( {{ $book_data['check_in'] }} -
-                                                        {{ $book_data['check_out'] }})</b></p>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <p> {{ $nights }} Days</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <p>Total Room</p>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <p>{{ $book_data['number_of_rooms'] }}</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <p>Subtotal</p>
-                                            </td>
-                                            <td style="text-align: right">
-                                                <p>${{ $subtotal }}</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <p>Discount</p>
-                                            </td>
-                                            <td style="text-align:right">
-                                                <p>${{ $discount }}</p>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <p>Total</p>
-                                            </td>
-                                            <td style="text-align:right">
-                                                <p>${{ $subtotal - $discount }}</p>
-                                            </td>
-                                        </tr>
-                                    </table>
-
-                                </div>
-                            </div>
-                        </section>
-
-                    </div>
-
-
-                    <div class="col-lg-8 col-md-8">
-                        <div class="payment-box">
-                            <div class="payment-method">
-
-                                <p>
-                                    <input type="radio" id="cash-on-delivery" name="payment_method" value="COD">
-                                    <label for="cash-on-delivery">Cash On Delivery</label>
-                                </p>
-                                <p>
-                                    <input type="radio" id="paypal" name="radio-group">
-                                    <label for="paypal">Stripe</label>
-                                </p>
-
-                            </div>
-                            <button type="submit" class="order-btn">Place to Order</button>
 
                         </div>
                     </div>
                 </div>
-            </form>
-        </div>
-    </section>
-    <!-- Checkout Area End -->
+
+
+                <div class="col-lg-4">
+                    <section class="checkout-area pb-70">
+                        <div class="card-body">
+                              <div class="billing-details">
+                                    <h3 class="title">Booking Summary</h3>
+                                    <hr>
+
+    <div style="display: flex">
+            <img style="height:100px; width:120px;object-fit: cover" src="{{ (!empty($room->image))? url('upload/roomimg/'.$room->image):url('upload/no_image.jpg') }}" alt="Images" alt="Images">
+            <div style="padding-left: 10px;">
+                <a href=" " style="font-size: 20px; color: #595959;font-weight: bold">{{ @$room->type->name }}</a>
+                <p><b>{{ $room->price }} / Night</b></p>
+            </div>
+
+    </div>
+
+                                    <br>
+
+    <table class="table" style="width: 100%">
+        @php
+      $subtotal = $room->price * $nights * $book_data['number_of_rooms'];
+      $discount =($room->discount/100)*$subtotal;
+        @endphp
+
+            <tr>
+                <td><p>Total Night <br> <b> ( {{ $book_data['check_in'] }} - {{ $book_data['check_out'] }})</b></p></td>
+                <td style="text-align: right"><p> {{ $nights }} Days</p></td>
+            </tr>
+            <tr>
+                <td><p>Total Room</p></td>
+                <td style="text-align: right"><p>{{ $book_data['number_of_rooms'] }}</p></td>
+            </tr>
+            <tr>
+                <td><p>Subtotal</p></td>
+                <td style="text-align: right"><p>${{ $subtotal }}</p></td>
+            </tr>
+            <tr>
+                <td><p>Discount</p></td>
+                <td style="text-align:right"> <p>${{ $discount }}</p></td>
+            </tr>
+            <tr>
+                <td><p>Total</p></td>
+                <td style="text-align:right"> <p>${{ $subtotal-$discount }}</p></td>
+            </tr>
+    </table>
+
+                              </div>
+                        </div>
+                  </section>
+
+                </div>
+
+
+                <div class="col-lg-8 col-md-8">
+                    <div class="payment-box">
+                        <div class="payment-method">
+
+            <p>
+   <input type="radio" id="cash-on-delivery" name="payment_method" value="COD">
+                <label for="cash-on-delivery">Cash On Delivery</label>
+            </p>
+            <p>
+                <input type="radio" id="paypal" name="radio-group">
+                <label for="paypal">Stripe</label>
+            </p>
+
+                        </div>
+       <button type="submit" class="order-btn">Place to Order</button>
+
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+<!-- Checkout Area End -->
+
+
+
 @endsection
