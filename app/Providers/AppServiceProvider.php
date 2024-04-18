604e396db2db1994020272b2012be028c93e99c0
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-19 01:17:12 +0700

    Dynamic Email Configuration Part 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // Thêm dòng này để import Schema
+use App\Models\SmtpSetting;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (Schema::hasTable('smtp_settings')) { // Sử dụng Schema ở đây
+           $smtpsetting = SmtpSetting::first();
+
+           if ($smtpsetting) {
+              $data = [
+                'driver' => $smtpsetting->mailer,
+                'host' => $smtpsetting->host,
+                'port' => $smtpsetting->port,
+                'username' => $smtpsetting->username,
+                'password' => $smtpsetting->password,
+                'encryption' => $smtpsetting->encryption,
+                'from' => [
+                    'address' => $smtpsetting->from_address,
+                    'name' => 'Easyhotel'
+                ]
+              ];
+              Config::set('mail',$data);
+           }
+
+        } // end if
     }
 }
