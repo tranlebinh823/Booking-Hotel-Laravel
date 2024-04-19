209b5799b1e3980836d84e6edb1fd6f26f4681b5
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-04-19 20:55:37 +0700

    Blog Post Setup Part 4

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -10,6 +10,7 @@ use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
 use Illuminate\Support\Facades\Auth;
+
 class BlogController extends Controller
 {
     public function BlogCategory()
@@ -82,19 +83,20 @@ class BlogController extends Controller
         $blogcat = BlogCategory::latest()->get();
         return view('backend.post.add_post', compact('blogcat'));
     } // End Method
-    public function StoreBlogPost(Request $request){
+    public function StoreBlogPost(Request $request)
+    {
 
         $image = $request->file('post_image');
-        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-        Image::make($image)->resize(550,370)->save('upload/post/'.$name_gen);
-        $save_url = 'upload/post/'.$name_gen;
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(550, 370)->save('upload/post/' . $name_gen);
+        $save_url = 'upload/post/' . $name_gen;
 
         BlogPost::insert([
 
             'blogcat_id' => $request->blogcat_id,
             'user_id' => Auth::user()->id,
             'post_titile' => $request->post_titile,
-            'post_slug' => strtolower(str_replace(' ','-',$request->post_titile)),
+            'post_slug' => strtolower(str_replace(' ', '-', $request->post_titile)),
             'short_descp' => $request->short_descp,
             'long_descp' => $request->long_descp,
             'post_image' => $save_url,
@@ -107,6 +109,85 @@ class BlogController extends Controller
         );
 
         return redirect()->route('all.blog.post')->with($notification);
+    } // End Method
+    public function EditBlogPost($id)
+    {
+
+        $blogcat = BlogCategory::latest()->get();
+        $post = BlogPost::find($id);
+        return view('backend.post.edit_post', compact('blogcat', 'post'));
+    } // End Method
+
+
+    public function UpdateBlogPost(Request $request)
+    {
+
+        $post_id = $request->id;
+
+        if ($request->file('post_image')) {
+
+            $image = $request->file('post_image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(550, 370)->save('upload/post/' . $name_gen);
+            $save_url = 'upload/post/' . $name_gen;
+
+            BlogPost::findOrFail($post_id)->update([
+
+                'blogcat_id' => $request->blogcat_id,
+                'user_id' => Auth::user()->id,
+                'post_titile' => $request->post_titile,
+                'post_slug' => strtolower(str_replace(' ', '-', $request->post_titile)),
+                'short_descp' => $request->short_descp,
+                'long_descp' => $request->long_descp,
+                'post_image' => $save_url,
+                'created_at' => Carbon::now(),
+            ]);
+
+            $notification = array(
+                'message' => 'BlogPost Updated With Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.blog.post')->with($notification);
+        } else {
+
+            BlogPost::findOrFail($post_id)->update([
+
+                'blogcat_id' => $request->blogcat_id,
+                'user_id' => Auth::user()->id,
+                'post_titile' => $request->post_titile,
+                'post_slug' => strtolower(str_replace(' ', '-', $request->post_titile)),
+                'short_descp' => $request->short_descp,
+                'long_descp' => $request->long_descp,
+                'created_at' => Carbon::now(),
+            ]);
+
+            $notification = array(
+                'message' => 'BlogPost Updated Without Image Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.blog.post')->with($notification);
+        } // End Eles
+
 
     } // End Method
+
+
+    public function DeleteBlogPost($id)
+    {
+
+        $item = BlogPost::findOrFail($id);
+        $img = $item->post_image;
+        unlink($img);
+
+        BlogPost::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'BlogPost Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }   // End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -150,6 +150,9 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
         Route::get('/all/blog/post', 'AllBlogPost')->name('all.blog.post');
         Route::get('/add/blog/post', 'AddBlogPost')->name('add.blog.post');
         Route::post('/store/blog/post', 'StoreBlogPost')->name('store.blog.post');
+        Route::get('/edit/blog/post/{id}', 'EditBlogPost')->name('edit.blog.post');
+        Route::post('/update/blog/post', 'UpdateBlogPost')->name('update.blog.post');
+        Route::get('/delete/blog/post/{id}', 'DeleteBlogPost')->name('delete.blog.post');
     });
 }); // End Admin Group Middleware
 
