10c4d7f1ec9bf2bcf8e83d3cfbcc1bbb1639529a
ResumeKTech <nganhkhoa.becloud@gmail.com>
2024-05-04 00:05:50 +0700

    Setup User Permission Part 2

--- app/Http/Controllers/Backend/RoleController.php
+++ app/Http/Controllers/Backend/RoleController.php
@@ -6,10 +6,17 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+
 class RoleController extends Controller
 {
-    public function AllPermission(){
+    public function AllPermission()
+    {
         $permissions = Permission::latest()->get();
-        return view('backend.pages.permission.all_permission',compact('permissions'));
+        return view('backend.pages.permission.all_permission', compact('permissions'));
     } // End Method
+    public function AddPermission()
+    {
+
+        return view('backend.pages.permission.add_permission');
+    } // End Method 
 }
